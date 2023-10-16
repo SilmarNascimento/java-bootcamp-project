d808fcfc229ff70d1c74dcdf4bc1d5685d899d86
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-16 19:22:44 -0300

    feat: implementa a class mapHTTPStatus

---
+++ src/main/java/com/silmarfnascimento/bootcampproject/utils/mapHTTPStatus.java
@@ -0,0 +1,16 @@
+package com.silmarfnascimento.bootcampproject.utils;
+
+import org.springframework.http.HttpStatus;
+
+public class mapHTTPStatus {
+  public static HttpStatus mapHttpStatus(String status) {
+    return switch (status) {
+      case "OK" -> HttpStatus.OK;
+      case "CREATED" -> HttpStatus.CREATED;
+      case "NOT_FOUND" -> HttpStatus.NOT_FOUND;
+      case "BAD_REQUEST" -> HttpStatus.BAD_REQUEST;
+      case "UNAUTHORIZED" -> HttpStatus.UNAUTHORIZED;
+      default -> HttpStatus.INTERNAL_SERVER_ERROR;
+    };
+  }
+}
