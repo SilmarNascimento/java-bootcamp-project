64f58eca76408ea2e202cd91b46e05fdf317282b
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-16 20:22:36 -0300

    feat: adiciona os endpoints de cada controller

--- src/main/java/com/silmarfnascimento/bootcampproject/controller/BlogPostController.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/controller/BlogPostController.java
@@ -1,4 +1,9 @@
 package com.silmarfnascimento.bootcampproject.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blogpost")
 public class BlogPostController {
 }
