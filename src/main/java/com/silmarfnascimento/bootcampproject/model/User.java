b725f68a0f233902fb5cba443bffd3ef98a94252
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-16 18:37:33 -0300

    feat: implementa a classe user

---
+++ src/main/java/com/silmarfnascimento/bootcampproject/model/User.java
@@ -0,0 +1,20 @@
+package com.silmarfnascimento.bootcampproject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  private String username;
+  private String email;
+  private String password;
+  private String image;
+}
