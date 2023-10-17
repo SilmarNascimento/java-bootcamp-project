78701ed7f69df6f761f994f65780de6fbbd960cc
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-17 14:21:55 -0300

    feat: implementa a class UserService

--- src/main/java/com/silmarfnascimento/bootcampproject/repository/IUserRepository.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/repository/IUserRepository.java
@@ -3,8 +3,9 @@ package com.silmarfnascimento.bootcampproject.repository;
 import com.silmarfnascimento.bootcampproject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IUserRepository extends JpaRepository<User, UUID> {
-  User findByUsername(String username);
+  Optional<User> findByUsername(String username);
 }
--- src/main/java/com/silmarfnascimento/bootcampproject/service/Implementation/UserService.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/service/Implementation/UserService.java
@@ -1,34 +1,72 @@
 package com.silmarfnascimento.bootcampproject.service.Implementation;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.silmarfnascimento.bootcampproject.model.User;
+import com.silmarfnascimento.bootcampproject.repository.IUserRepository;
 import com.silmarfnascimento.bootcampproject.service.IUserService;
 import com.silmarfnascimento.bootcampproject.service.ServiceResponse;
+import com.silmarfnascimento.bootcampproject.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class UserService implements IUserService {
+  @Autowired
+  private IUserRepository userRepository;
+
   @Override
   public ServiceResponse findAll() {
-    return null;
+    List<User> users = userRepository.findAll();
+    return new ServiceResponse("OK", users);
   }
 
   @Override
   public ServiceResponse findById(UUID id) {
-    return null;
+    Optional<User> user = userRepository.findById(id);
+    if(user.isEmpty()) {
+      return new ServiceResponse("NOT_FOUND", "Usuário não encontrado");
+    }
+    return new ServiceResponse("OK", user);
   }
 
   @Override
   public ServiceResponse create(User user) {
-    return null;
+    Optional<User> userFound = this.userRepository.findByUsername(user.getUsername());
+    if (userFound.isPresent()) {
+      return new ServiceResponse("BAD_REQUEST", "Usuário já existente!");
+    }
+    hashUserPassword(user);
+    User createdUser = userRepository.save(user);
+    return new ServiceResponse("CREATED", createdUser);
   }
 
   @Override
   public ServiceResponse update(UUID id, User user) {
-    return null;
+    Optional<User> userFound = userRepository.findById(id);
+    if (userFound.isEmpty()) {
+      return new ServiceResponse("NOT_FOUND", "Client not Found");
+    }
+
+    var passwordVerify = BCrypt.verifyer().verify(user.getPassword().toCharArray(), userFound.get().getPassword());
+    if(!passwordVerify.verified) {
+      hashUserPassword(user);
+    }
+
+    Utils.copyNonNullProperties(user, userFound.get());
+    User updatedUser = userRepository.save(user);
+    return new ServiceResponse("OK", updatedUser);
   }
 
   @Override
   public void delete(UUID id) {
+    userRepository.deleteById(id);
+  }
 
+  private static void hashUserPassword(User user) {
+    String passwordHashed = BCrypt.withDefaults()
+        .hashToString(12, user.getPassword().toCharArray());
+    user.setPassword(passwordHashed);
   }
 }
