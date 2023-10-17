132799d3e8ab06eda2c9ec58ff81cca7e2bc43a3
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-17 19:22:37 -0300

    feat: adiciona mais status HTTP à classe mapHTTPStatus

--- src/main/java/com/silmarfnascimento/bootcampproject/utils/mapHTTPStatus.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/utils/mapHTTPStatus.java
@@ -7,9 +7,11 @@ public class mapHTTPStatus {
     return switch (status) {
       case "OK" -> HttpStatus.OK;
       case "CREATED" -> HttpStatus.CREATED;
-      case "NOT_FOUND" -> HttpStatus.NOT_FOUND;
+      case "NO_CONTENT" -> HttpStatus.NO_CONTENT;
       case "BAD_REQUEST" -> HttpStatus.BAD_REQUEST;
       case "UNAUTHORIZED" -> HttpStatus.UNAUTHORIZED;
+      case "FORBIDDEN" -> HttpStatus.FORBIDDEN;
+      case "NOT_FOUND" -> HttpStatus.NOT_FOUND;
       default -> HttpStatus.INTERNAL_SERVER_ERROR;
     };
   }
