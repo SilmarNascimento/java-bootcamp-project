458899e8f35d1258a5100d29993fc64e2251501a
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-17 10:17:21 -0300

    feat: implementa a CategoryController

--- src/main/java/com/silmarfnascimento/bootcampproject/controller/CategoryController.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/controller/CategoryController.java
@@ -1,9 +1,54 @@
 package com.silmarfnascimento.bootcampproject.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.silmarfnascimento.bootcampproject.model.Category;
+import com.silmarfnascimento.bootcampproject.service.ICategoryService;
+import com.silmarfnascimento.bootcampproject.service.ServiceResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+import static com.silmarfnascimento.bootcampproject.utils.mapHTTPStatus.mapHttpStatus;
 
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
+  @Autowired
+  private ICategoryService categoryService;
+
+  @GetMapping
+  public ResponseEntity<Object> findAllCategories() {
+    ServiceResponse serviceResponse = categoryService.findAll();
+    return ResponseEntity.status(mapHttpStatus(serviceResponse.getStatus())).body(serviceResponse.getData());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> findCategoryById(UUID id) {
+    ServiceResponse serviceResponse = categoryService.findById(id);
+    if(serviceResponse.getData() != null) {
+      return ResponseEntity.status(mapHttpStatus(serviceResponse.getStatus())).body(serviceResponse.getData());
+    }
+    return ResponseEntity.status(mapHttpStatus(serviceResponse.getStatus())).body("Cliente não encontrado");
+  }
+
+  @PostMapping
+  public ResponseEntity<Object> createCategory(@RequestBody @Validated Category category) {
+    ServiceResponse serviceResponse = categoryService.create(category);
+    return ResponseEntity.status(mapHttpStatus(serviceResponse.getStatus())).body(serviceResponse.getData());
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Object> updateCategory(@PathVariable UUID id, @RequestBody Category category) {
+    ServiceResponse serviceResponse = categoryService.update(id, category);
+    return ResponseEntity.status(mapHttpStatus(serviceResponse.getStatus())).body(serviceResponse.getData());
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteCategory(@PathVariable UUID id) {
+    categoryService.delete(id);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
 }
