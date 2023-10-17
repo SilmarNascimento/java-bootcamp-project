8c8b625cfd8431a88fd3fc3791aaf3e632fdee7f
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-17 09:36:22 -0300

    feat: implementa a class UserController

--- src/main/java/com/silmarfnascimento/bootcampproject/controller/UserController.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/controller/UserController.java
@@ -1,9 +1,54 @@
 package com.silmarfnascimento.bootcampproject.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.silmarfnascimento.bootcampproject.model.User;
+import com.silmarfnascimento.bootcampproject.service.IUserService;
+import com.silmarfnascimento.bootcampproject.service.ServiceResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+import static com.silmarfnascimento.bootcampproject.utils.mapHTTPStatus.mapHttpStatus;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
+  @Autowired
+  private IUserService userService;
+
+  @GetMapping
+  public ResponseEntity<Object> findAllUsers() {
+    ServiceResponse serviceResponse = userService.findAll();
+    return ResponseEntity.status(mapHttpStatus(serviceResponse.getStatus())).body(serviceResponse.getData());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> findUserById(UUID id) {
+    ServiceResponse serviceResponse = userService.findById(id);
+    if(serviceResponse.getData() != null) {
+      return ResponseEntity.status(mapHttpStatus(serviceResponse.getStatus())).body(serviceResponse.getData());
+    }
+    return ResponseEntity.status(mapHttpStatus(serviceResponse.getStatus())).body("Cliente não encontrado");
+  }
+
+  @PostMapping
+  public ResponseEntity<Object> registerUser(@RequestBody @Validated User user) {
+    ServiceResponse serviceResponse = userService.create(user);
+    return ResponseEntity.status(mapHttpStatus(serviceResponse.getStatus())).body(serviceResponse.getData());
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Object> updateUser(@PathVariable UUID id, @RequestBody User user) {
+    ServiceResponse serviceResponse = userService.update(id, user);
+    return ResponseEntity.status(mapHttpStatus(serviceResponse.getStatus())).body(serviceResponse.getData());
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+    userService.delete(id);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
 }
