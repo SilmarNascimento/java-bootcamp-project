1544f6fe6407dafa839d4892eaa4959558812748
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-29 16:11:52 -0300

    implementa as rotas que são livres e as rotas que precisam de autenticação

--- src/main/java/com/silmarfnascimento/bootcampproject/security/SecurityConfig.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.silmarfnascimento.bootcampproject.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,7 +11,14 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable());
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> {
+              auth
+                  .requestMatchers(HttpMethod.POST, "/users/").permitAll()
+                  .requestMatchers(HttpMethod.POST, "/auth/login").permitAll();
+              auth.anyRequest().authenticated();
+            }
+        );
     return http.build();
   }
 }
