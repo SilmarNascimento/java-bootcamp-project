44e7c54eb06ae2874d2cc490fa2c4460a642388e
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-30 18:55:46 -0300

    feat: adiciona javadoc para a classe utils

--- src/main/java/com/silmarfnascimento/bootcampproject/utils/Utils.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/utils/Utils.java
@@ -8,11 +8,25 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 
+/**
+ * Class Utils - It contains methods to help update an object field even with null properties.
+ */
 public class Utils {
+
+  /**
+   * Static Method - Merge all attributes from a source object into the target object in order to
+   * update all fields even if there are some null fields.
+   */
   public static void copyNonNullProperties(Object source, Object target) {
     BeanUtils.copyProperties(source, target, getNullPropertyName(source));
   }
 
+  /**
+   * Static Method - Map an array with the name of all null properties from the object.
+   *
+   * @param source - An object which may contains null properties.
+   * @return - returns an array with the name of all null properties from the object.
+   */
   public static String[] getNullPropertyName(Object source) {
     final BeanWrapper src = new BeanWrapperImpl(source);
     PropertyDescriptor[] pds = src.getPropertyDescriptors();
