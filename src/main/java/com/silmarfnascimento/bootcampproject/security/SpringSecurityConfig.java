3f190e3ac4b7fde1037a1ae34c376e5773405b01
SilmarNascimento <silmarfnascimento@gmail.com>
2023-10-31 16:26:56 -0300

    fix: implementa a correção da configuração da SpringSecurityConfig

--- src/main/java/com/silmarfnascimento/bootcampproject/security/SpringSecurityConfig.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/security/SpringSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -16,13 +18,19 @@ public class SpringSecurityConfig {
   private JWTFilter jwtFilter;
 
   @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+    return new MvcRequestMatcher.Builder(introspector);
+  }
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc)
+      throws Exception {
     http.csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> {
               auth
-                  .requestMatchers(HttpMethod.POST, "/users/").permitAll()
-                  .requestMatchers(HttpMethod.POST, "/auth/login").permitAll();
-              auth.anyRequest().authenticated();
+                  .requestMatchers(mvc.pattern(HttpMethod.POST, "/users/")).permitAll()
+                  .requestMatchers(mvc.pattern(HttpMethod.POST, "/auth/login")).permitAll()
+                  .anyRequest().authenticated();
             }
         )
         .addFilterBefore(jwtFilter, BasicAuthenticationFilter.class);
