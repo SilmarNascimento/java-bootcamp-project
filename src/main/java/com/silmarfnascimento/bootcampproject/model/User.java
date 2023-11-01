78b7f8e85dce317b24fe3fc9491f6b9b0b38516f
SilmarNascimento <silmarfnascimento@gmail.com>
2023-11-01 12:22:11 -0300

    feat: implementa os métodos do UserDetails na classe User

--- src/main/java/com/silmarfnascimento/bootcampproject/model/User.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/model/User.java
@@ -4,13 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import java.util.Collection;
+import java.util.List;
 import lombok.Data;
 
 import java.util.UUID;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
 @Entity(name = "tb_users")
-public class User {
+public class User extends UserDetails {
 
   @Id
   @GeneratedValue(generator = "UUID")
@@ -21,4 +25,29 @@ public class User {
   private String email;
   private String password;
   private String image;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
