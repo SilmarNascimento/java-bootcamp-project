ae3790792686ee5771566025deb5a776660f3a03
SilmarNascimento <silmarfnascimento@gmail.com>
2023-11-08 14:54:21 -0300

    feat: adiciona ao swagger a configuração da autenticação para indicar e facilitar o uso do token nas requisições

--- src/main/java/com/silmarfnascimento/bootcampproject/BootcampProjectApplication.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/BootcampProjectApplication.java
@@ -1,13 +1,17 @@
 package com.silmarfnascimento.bootcampproject;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "BlogPostApi", version = "1", description = "API desenvolvida para um Blog"))
+@SecurityScheme(name = "jwt_authentication", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class BootcampProjectApplication {
 
   public static void main(String[] args) {
