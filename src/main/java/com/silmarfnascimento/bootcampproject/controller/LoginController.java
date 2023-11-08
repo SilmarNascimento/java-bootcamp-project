8c2550e1dfd14edb74d03ad485d7dbac7aed1a0f
SilmarNascimento <silmarfnascimento@gmail.com>
2023-11-08 15:15:22 -0300

    feat: implementa a configuração exemplo da camada controller habilitando o swagger de ter uma interface para facilitar o envio do token para autenticação

--- src/main/java/com/silmarfnascimento/bootcampproject/controller/LoginController.java
+++ src/main/java/com/silmarfnascimento/bootcampproject/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.silmarfnascimento.bootcampproject.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import com.silmarfnascimento.bootcampproject.dto.Login;
@@ -18,13 +21,17 @@ import static com.silmarfnascimento.bootcampproject.utils.mapHTTPStatus.mapHttpS
 
 @RestController
 @RequestMapping("/auth")
-@Tag(name = "LoginController")
+@Tag(name = "Login", description = "Rota para Login de usuários")
 public class LoginController {
 
   @Autowired
   private LoginService loginService;
 
   @Operation(summary = "Realiza o login do usuário", method = "POST")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "OK")
+  })
+  @SecurityRequirement(name = "jwt_authentication")
   @PostMapping("/login")
   public ResponseEntity<Object> login(@RequestBody @Valid Login login) {
     System.out.println("entrei loginController");
